f67e6b04b6a93f3022b9c87df809c2ad3a734196
Steven118 <steven118@live.com>
2025-11-15 07:49:41 +0800

    change update url and reset small version to 001.R

--- constants/UpdateConfig.ts
+++ constants/UpdateConfig.ts
@@ -7,12 +7,12 @@ export const UPDATE_CONFIG = {
 
   // GitHub相关URL
   GITHUB_RAW_URL:
-    `https://ghfast.top/https://raw.githubusercontent.com/steven118-live/OrionTV/refs/heads/master/package.json?t=${Date.now()}`,
+    `https://ghfast.top/https://raw.githubusercontent.com/steven118-live/OrionTV_R/refs/heads/master/package.json?t=${Date.now()}`,
   ORIONTV_ORG_GITHUB_RAW_URL:
     `https://ghfast.top/https://raw.githubusercontent.com/orion-lib/OrionTV/refs/heads/master/package.json?t=${Date.now()}`,
   // 获取平台特定的下载URL
   getDownloadUrl(version: string): string {
-    return `https://ghfast.top/https://github.com/steven118-live/OrionTV/releases/download/v${version}/orionTV.${version}.apk`;
+    return `https://ghfast.top/https://github.com/steven118-live/OrionTV_R/releases/download/v${version}/orionTV.${version}.apk`;
   },
 
   // 是否显示更新日志
