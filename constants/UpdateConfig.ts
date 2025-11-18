d7428a527e113d896a1c8b5001aea5b213edc2ef
Steven118 <steven118@live.com>
2025-11-18 21:34:04 +0800

    chore: initial import OrionTVR v1.3.11.001

--- constants/UpdateConfig.ts
+++ constants/UpdateConfig.ts
@@ -9,7 +9,7 @@ export const UPDATE_CONFIG = {
   ORIONTV_ORG_GITHUB_RAW_URL:
     `https://ghfast.top/https://raw.githubusercontent.com/orion-lib/OrionTV/refs/heads/master/package.json?t=${Date.now()}`,
   GITHUB_RAW_URL:
-    `https://ghfast.top/https://raw.githubusercontent.com/steven118-live/OrionTVR/main/package.json?t=${Date.now()}`,
+    `https://ghfast.top/https://raw.githubusercontent.com/steven118-live/OrionTVR/master/package.json?t=${Date.now()}`,
 // 获取平台特定的下载URL
   getDownloadUrl(version: string): string {
     return `https://ghfast.top/https://github.com/steven118-live/OrionTVR/releases/download/v${version}/orionTV.${version}.apk`;
